63f9b26234e79d3912fa3f33dc6c7c4fece282ac
nathalia <nathaliamilenic@gmail.com>
2026-05-27 17:08:23 -0300

    divisao de paginas

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('visao/', views.visao, name="visao"),
+    path('elenco/', views.elenco, name="elenco"),
+    path('sobre/', views.sobre, name="sobre"),
+
 ]
--- app/views.py
+++ app/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'app/index.html')
+
+def visao(request):
+    return render(request, 'app/visao.html')
+
+def elenco(request):
+    return render(request, 'app/elenco.html')
+
+def sobre(request):
+    return render(request, 'app/sobre.html')
